17c7f948e829bc022bcf6f47aa6a1e119961e828
Pugsly99 <rylandadams@yahoo.com>
2021-02-12 13:28:19 -0800

    keg list

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,6 @@ function Header(){
     <>
       <h2>Welcome to the TAP HOUSE</h2>
       <img src="https://n3taphouse.com/wp-content/uploads/2017/07/taps3-e1512199746832.jpg" width= "400" alt="BAR"/>
-      <hr/>   
     </>
   )
 }
--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -5,8 +5,47 @@ import PropTypes from 'prop-types';
 function NewKegForm(props){
   return (
     <>
+      <form onSubmit = {handleNewKegFormSubmission}>
+        <input
+          type = "text"
+          name = "name"
+          placeholder = "Keg Name"
+          required/>
+        <input
+          type = "text"
+          name = "brand"
+          placeholder = "Brand Name"
+          required/>
+        <input
+          type = "text"
+          name = "price"
+          placeholder = "$0.00"
+          required/>
+        <input
+          type = "text"
+          name = "Alcohol Content"
+          placeholder = "$0.00"
+          required/>
+        <input
+          type='number'
+          name='quantity'
+          placeholder="0"
+          min='0'
+          required />
+        <button type='submit'>Create New Keg</button>
+      </form>
     </>
   );
+
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    props.onNewFormCreation({name: event.target.name.value, brand: event.target.brand.value, price: event.target.price.value, alcoholContent: event.target.alcoholContent.value, quantity: parseInt(event.target.quantity.value), id: v4()
+    });
+  }
+}
+
+NewKegForm.propTypes = {
+  onNewKegCreation: PropTypes.func
 }
 
 export default NewKegForm;
