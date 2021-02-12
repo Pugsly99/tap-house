db1d033aa49930bc96c26c366e9bee24bed487b2
Pugsly99 <rylandadams@yahoo.com>
2021-02-12 11:09:27 -0800

    sharedview

--- src/components/SharedView.js
+++ src/components/SharedView.js
@@ -40,7 +40,51 @@ class SharedView extends React.Component {
     }
   }
 
-  
+  handleAddingNewKegToList = (newKeg) => {
+    const newMasterKegList = this.state.masterKegList.concat(newKeg);
+    this.setState({masterKegList: newMasterKegList,
+                    visibleView: 1});
+  }
+
+  handleChangingSelectedKeg = (id) => {
+    const selectedKeg = this.state.masterKegList.filter(keg => keg.id === id)[0];
+    this.setstate({selectedKeg: selectedKeg});
+  }
+
+  handleRestock = () => {
+    const quantityOfKeg = this.state.selectedKeg.quantity;
+    const restockedKeg = {...this.state.selectedKeg, quantity: (quantityOfKeg + 1)};
+    const newMasterList = this.state.masterKegList.filter(keg => keg.id !== this.state.selectedKeg.id).concat(restockedKeg);
+    this.setState({selectedKeg: restockedKeg, masterKegList: newMasterList});
+  }
+
+  handleBuy = () => {
+    const quantityOfKeg = this.state.selectedKeg.quantity;
+    const restockedKeg = {...this.state.selectedKeg, quantity: (quantityOfKeg -1)};
+    const newMasterList = this.state.masterKegList.filter(keg => keg.id !== this.state.selectedKeg.id).concat(restockedKeg);
+    this.setState({selectedKeg: restockedKeg, masterKegList: newMasterList});
+  }
+
+  render(){
+    let currentlyVisibleState = null;
+    if (this.state.selectedKeg != null) {
+      currentlyVisibleState = <KegDetail keg = {this.state.selectedKeg} onRestock = {this.handleRestock} onBuy = {this.handleBuy}/>
+    } else if (this.state.visibleView === 0) {
+      currentlyVisibleState = null;
+    } else if (this.state.visibleView === 1) {
+      currentlyVisibleState = <KegList kegList = {this.state.masterKegList} onKegSelection = {this.handleChangingSelectedKeg}/>
+    } else if (this.state.visibleView === 2) {
+      currentlyVisibleState = <NewKegForm onNewKegCreation = {this.handleAddingNewKegToList}/>
+    }
+
+    return (
+      <>
+        <button onClick = {this.listClick}> List of Kegs </button>
+        <button onCLick = {this.newKegClick}> Create new Item </button>
+        {currentlyVisibleState}
+      </>
+    );
+  }
 }
 
 export default SharedView;
